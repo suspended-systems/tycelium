9dec4a8a80aba2dd0fe50cea062abe473b4459aa
Tony Natelli <tony@natel.li>
2025-01-15 13:57:50 -0800

    fix: 0.0.2

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reverseOneToManyDictionary = void 0;
-var reverseOneToManyDictionary_1 = require("./reverseOneToManyDictionary");
-Object.defineProperty(exports, "reverseOneToManyDictionary", { enumerable: true, get: function () { return reverseOneToManyDictionary_1.reverseOneToManyDictionary; } });
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { reverseOneToManyDictionary } from "./reverseOneToManyDictionary";
